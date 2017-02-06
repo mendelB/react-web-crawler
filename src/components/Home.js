285c501bac5095511c50bb94d543f757afdbb9ae
mendelB <mendelblesofsky@gmail.com>
2017-02-06 16:11:10 -0500

    more initial setup

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Home extends React.Component {
+	render() {
+		return (
+			<div>
+				Hey
+			</div>
+		)
+	}
+}
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 import App from './App';
+import Home from './components/Home';
 import './index.css';
 
 ReactDOM.render((
