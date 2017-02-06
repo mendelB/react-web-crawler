5d8bcd325eaeaf210f6742a056de9b1c2bad93d9
mendelB <mendelblesofsky@gmail.com>
2017-02-06 16:06:57 -0500

    initial setup

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory, IndexRoute } from 'react-router'
+import App from './App';
+import './index.css';
+
+ReactDOM.render((
+	<Router history={browserHistory}>
+		<Route path="/" component={ App }>
+			<IndexRoute component={ Home }/>
+		</Route>
+	</Router>
+),  document.getElementById('root'));
