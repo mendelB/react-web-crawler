342cc794b282887d6208104f1b5bdf7c5bcaf35a
mendelB <mendelblesofsky@gmail.com>
2017-03-10 15:22:23 -0500

    add frame for crawler component

---
+++ src/components/Crawler.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Crawler extends React.Component {
+	render() {
+		return (
+			<div className="Crawler">
+			</div>
+		)
+	}
+}
+
+export default Crawler;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 import App from './App';
 import Home from './components/Home';
+import Crawler from './components/Crawler'
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
@@ -11,6 +12,7 @@ ReactDOM.render((
 	<Router history={browserHistory}>
 		<Route path="/" component={ App }>
 			<IndexRoute component={ Home }/>
+			<Route path="crawl" component={ Crawler }/>
 		</Route>
 	</Router>
 ),  document.getElementById('root'));
